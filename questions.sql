b8e39a2bbca39928d1087df85eb8170a3fb0889a
Leonardo Rebonatto <55560467+LRebonatto@users.noreply.github.com>
2023-08-16 15:54:24 -0300

    Update questions.sql

--- questions.sql
+++ questions.sql
@@ -1,7 +1,9 @@
 1  Listar os empregados (nomes) que tem salário maior que seu chefe (usar o join)
 
-SELECT e1.nome FROM empregados e1 
-JOIN empregados e2 ON e1.supervisor_id=e2.emp_id where e1.salario > e2.salario;
+SELECT e1.nome
+FROM empregados e1 
+JOIN empregados e2 ON e1.supervisor_id=e2.emp_id
+WHERE e1.salario > e2.salario;
 
 --  empregado | 
 -- -----------+
@@ -49,16 +51,11 @@ WHERE e.salario = ms.max;
 
 4 Listar os nomes departamentos que tem menos de 3 empregados;
 
-//TODO finish this
-With LessThree AS
-    (
-            SELECT e.dep_id, count(e.dep_id) FROM empregados e JOIN departamentos d ON e.dep_id=d.dep_id GROUP BY e.dep_id 
-    )
-SELECT *
-FROM empregados e 
-JOIN LessThree ms ON e.dep_id=ms.dep_id
-WHERE ms.count < 3;
-
+SELECT e.dep_id, count(e.dep_id), d.nome
+FROM empregados e
+JOIN departamentos d ON e.dep_id=d.dep_id
+GROUP BY e.dep_id, d.nome
+HAVING e.count < 3;
 
 --    nome    
 -- -----------
@@ -69,6 +66,10 @@ WHERE ms.count < 3;
 
 5 Listar os departamentos  com o número de colaboradores
 
+SELECT d.nome, count(e.dep_id)
+FROM empregados e
+JOIN departamentos d ON e.dep_id=d.dep_id
+GROUP BY d.nome;
     
 --    nome    | count 
 -- -----------+-------
@@ -80,6 +81,11 @@ WHERE ms.count < 3;
 
 6 Listar os empregados que não possue o seu  chefe no mesmo departamento/ 
 
+SELECT e1.nome, e1.dep_id
+FROM empregados e1
+JOIN empregados e2 ON e1.supervisor_id = e2.emp_id
+WHERE e1.dep_id != e2.dep_id 
+    
 --  nome | dep_id 
 -- ------+--------
 --  Joao |      3
@@ -88,6 +94,12 @@ WHERE ms.count < 3;
 
 7 Listar os nomes dos  departamentos com o total de salários pagos (sliding windows function)
 
+SELECT DISTINCT
+    SUM(e.salario) OVER (PARTITION BY e.dep_id),
+	d.nome
+FROM empregados e
+JOIN departamentos d ON e.dep_id = d.dep_id;
+    
 --   sum  |   nome    
 -- -------+-----------
 --   6000 | Vendas
@@ -99,6 +111,17 @@ WHERE ms.count < 3;
 
 8 Listar os nomes dos colaboradores com salario maior que a média do seu departamento (dica: usar subconsultas);
 
+SELECT nome, salario
+FROM (
+    SELECT
+        e.nome,
+        e.salario,
+        AVG(e.salario) OVER (PARTITION BY e.dep_id) AS media
+    FROM empregados e
+    JOIN departamentos d ON e.dep_id = d.dep_id
+) AS subquery
+WHERE salario > media;
+
 --   Nome   | Salário 
 -- ---------+---------
 --  Maria   |    9500
@@ -106,6 +129,17 @@ WHERE ms.count < 3;
 --  Luiz    |    8000
 
  9  Faça uma consulta capaz de listar os dep_id, nome, salario, e as médias de cada departamento utilizando o windows function;
+
+SELECT *
+FROM (
+    SELECT
+		e.dep_id,
+        e.nome,
+        e.salario,
+        AVG(e.salario) OVER (PARTITION BY e.dep_id) AS media
+    FROM empregados e
+    JOIN departamentos d ON e.dep_id = d.dep_id
+) AS subquery
 
 --  dep_id |   nome    | salario |  avg  
 -- --------+-----------+---------+-------
@@ -122,6 +156,18 @@ WHERE ms.count < 3;
 
 
 10 - Encontre os empregados com salario maior ou igual a média do seu departamento. Deve ser reportado o salario do empregado e a média do departamento (dica: usar window function com subconsulta)
+
+SELECT *
+FROM (
+	SELECT
+        e.nome,
+        e.salario,
+		e.dep_id,
+        AVG(e.salario) OVER (PARTITION BY e.dep_id) AS media
+    FROM empregados e
+    JOIN departamentos d ON e.dep_id = d.dep_id
+) AS subquery
+WHERE salario >= media;
 
 --   nome   | salario | dep_id |       avg_salary       
 -- ---------+---------+--------+------------------------
